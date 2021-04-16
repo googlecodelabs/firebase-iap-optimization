af10bc1ba85b106bb908df9033867912b9cd4827
elvissunlihe@gmail.com <elvissunlihe@gmail.com>
2021-04-16 11:16:09 -0400

    Initial commit

---
+++ app/app/src/main/java/org/tensorflow/lite/examples/iapoptimizer/IapOptimizer.kt
@@ -0,0 +1,168 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.tensorflow.lite.examples.iapoptimizer
+
+import android.content.Context
+import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks.call
+import org.json.JSONObject
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.metadata.MetadataExtractor
+import java.io.File
+import java.io.RandomAccessFile
+import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class IapOptimizer(private val context: Context) {
+  val testInput = mapOf(
+    "coins_spent" to                       2048f,
+    "distance_avg" to                      1234f,
+    "device_os" to                         "ANDROID",
+    "game_day" to                          10f,
+    "geo_country" to                       "Canada",
+    "last_run_end_reason" to               "laser"
+  )
+
+  private var interpreter: Interpreter? = null
+  private var preprocessingSummary : JSONObject? = null
+  var isInitialized = false
+    private set
+
+  /** Executor to run inference task in the background */
+  private val executorService: ExecutorService = Executors.newCachedThreadPool()
+
+  private var modelInputSize: Int = 0 // will be inferred from TF Lite model
+
+  fun initialize(model: File): Task<Void> {
+    return call(
+      executorService,
+      Callable<Void> {
+        initializeInterpreter(model)
+        null
+      }
+    )
+  }
+
+  private fun initializeInterpreter(model: File) {
+    // Initialize TF Lite Interpreter with NNAPI enabled
+    val options = Interpreter.Options()
+    options.setUseNNAPI(true)
+    val interpreter: Interpreter
+
+    interpreter = Interpreter(model, options)
+
+    val aFile = RandomAccessFile(model, "r")
+
+    val inChannel: FileChannel = aFile.getChannel()
+    val fileSize: Long = inChannel.size()
+
+    val buffer = ByteBuffer.allocate(fileSize.toInt())
+    inChannel.read(buffer)
+    buffer.flip()
+
+    val metaExecutor = MetadataExtractor(buffer)
+
+    val inputStream = metaExecutor.getAssociatedFile("preprocess.json")
+    val inputAsString = inputStream.bufferedReader().use { it.readText() }
+
+    preprocessingSummary = JSONObject(inputAsString)
+
+    // Read input shape from model file
+    val inputShape = interpreter.getInputTensor(0).shape()
+
+    modelInputSize = inputShape[1]
+
+    // Finish interpreter initialization
+    this.interpreter = interpreter
+    isInitialized = true
+    Log.d(TAG, "Initialized TFLite interpreter.")
+  }
+
+  fun predict(): String {
+    if (!isInitialized) {
+      throw IllegalStateException("TF Lite Interpreter is not initialized yet.")
+    }
+
+    val preprocessedInput = preprocessModelInput(testInput)
+    val byteBuffer = Array(1) { preprocessedInput }
+
+    val result = Array(1) { FloatArray(OUTPUT_ACTIONS_COUNT) }
+    interpreter?.run(byteBuffer, result)
+
+    return mapOutputToAction(result[0])
+  }
+
+  private fun preprocessModelInput(input : Map<String, Any>) : FloatArray {
+    val result = mutableListOf<Float>()
+    for ((k, v) in input) {
+      if(!preprocessingSummary!!.has(k)) {
+        continue
+      }
+      val channelSummary = preprocessingSummary!!.getJSONObject(k)
+
+      if (channelSummary.getString("type") == "numerical") {
+        val mean =channelSummary.getDouble("mean")
+        val std =channelSummary.getDouble("std")
+        if (v is Float) {
+          result.add((v - mean.toFloat())/std.toFloat())
+        } else {
+          throw Exception("Invalid input")
+        }
+      }
+
+      if (channelSummary.getString("type") == "categorical") {
+        val allValues = channelSummary.getJSONArray("all_values")
+        for (i in 0 until allValues.length()) {
+          val possibleValue = allValues.getString(i)
+          if (v == possibleValue) {
+            result.add(1f)
+          } else {
+            result.add(0f)
+          }
+        }
+      }
+    }
+    return result.toFloatArray()
+  }
+
+  private fun mapOutputToAction(output: FloatArray) : String {
+    val mapping = preprocessingSummary!!.getJSONArray("output_mapping")
+
+    val maxIndex = output.indexOf(output.max()!!)
+
+    return "The best power-up to suggest: ${mapping[maxIndex] as String}"
+  }
+
+  fun close() {
+    call(
+      executorService,
+      Callable<String> {
+        interpreter?.close()
+        Log.d(TAG, "Closed TFLite interpreter.")
+        null
+      }
+    )
+  }
+
+  companion object {
+    private const val TAG = "IapOptimizer"
+
+    private const val OUTPUT_ACTIONS_COUNT = 8
+  }
+}
---
+++ app/app/src/main/java/org/tensorflow/lite/examples/iapoptimizer/MainActivity.kt
@@ -0,0 +1,102 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.tensorflow.lite.examples.iapoptimizer
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager
+import com.google.firebase.ml.custom.FirebaseCustomRemoteModel
+
+
+class MainActivity : AppCompatActivity() {
+
+  private var predictButton: Button? = null
+  private var predictedTextView: TextView? = null
+  private var iapOptimizer = IapOptimizer(this)
+
+  @SuppressLint("ClickableViewAccessibility")
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.tfe_dc_activity_main)
+
+    predictButton = findViewById(R.id.predict_button)
+    predictedTextView = findViewById(R.id.predicted_text)
+    predictedTextView?.text = "Click predict to see prediction result"
+
+    predictButton?.setOnClickListener {
+      val result = iapOptimizer.predict()
+      predictedTextView?.text = result
+    }
+
+    downloadModel("optimizer")
+  }
+
+  private fun downloadModel(modelName: String): Task<Void> {
+    val remoteModel = FirebaseCustomRemoteModel.Builder(modelName).build()
+    val firebaseModelManager = FirebaseModelManager.getInstance()
+    return firebaseModelManager
+      .isModelDownloaded(remoteModel)
+      .continueWithTask { task ->
+        // Create update condition if model is already downloaded, otherwise create download
+        // condition.
+        val conditions = if (task.result != null && task.result == true) {
+          FirebaseModelDownloadConditions.Builder()
+            .requireWifi()
+            .build() // Update condition that requires wifi.
+        } else {
+          FirebaseModelDownloadConditions.Builder().build(); // Download condition.
+        }
+        firebaseModelManager.download(remoteModel, conditions)
+      }
+      .addOnSuccessListener {
+        firebaseModelManager.getLatestModelFile(remoteModel)
+          .addOnCompleteListener {
+            val model = it.result
+            if (model == null) {
+              showToast("Failed to get model file.")
+            } else {
+              showToast("Downloaded remote model: $modelName")
+              iapOptimizer.initialize(model)
+            }
+          }
+      }
+      .addOnFailureListener {
+        showToast("Model download failed for $modelName, please check your connection.")
+      }
+  }
+
+  override fun onDestroy() {
+    iapOptimizer.close()
+    super.onDestroy()
+  }
+
+  private fun showToast(text: String) {
+    Toast.makeText(
+            this,
+            text,
+            Toast.LENGTH_LONG
+    ).show()
+  }
+
+  companion object {
+    private const val TAG = "MainActivity"
+  }
+}
